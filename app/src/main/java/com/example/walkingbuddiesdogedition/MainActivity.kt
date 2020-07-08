26910980119170bc500bb97590862656f49ad8aa
denisseai <msdenisseanaya@gmail.com>
2020-07-07 21:25:55 -0700

    initial commit

---
+++ app/src/main/java/com/example/walkingbuddiesdogedition/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.walkingbuddiesdogedition
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.lorentzos.flingswipe.SwipeFlingAdapterView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private var al = ArrayList<String>()
+    private var arrayAdapter: ArrayAdapter<String>? = null
+    private var i = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        al.add("dog1")
+        al.add("dog2")
+        al.add("dog3")
+        al.add("dog4")
+
+        arrayAdapter = ArrayAdapter(this, R.layout.item, R.id.helloText, al )
+
+        //set the listener and the adapter
+        frame.adapter = arrayAdapter
+        frame.setFlingListener(object: SwipeFlingAdapterView.onFlingListener {
+            override fun removeFirstObjectInAdapter() {
+                Log.d("LIST", "removed object!")
+                al.removeAt(0)
+                arrayAdapter?.notifyDataSetChanged()
+            }
+
+            override fun onLeftCardExit(p0: Any?) {
+                Toast.makeText(this@MainActivity, "Nah!", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onRightCardExit(p0: Any?) {
+                Toast.makeText(this@MainActivity, "Yes!", Toast.LENGTH_SHORT).show()
+
+            }
+
+            override fun onAdapterAboutToEmpty(p0: Int) {
+                al.add("XML $i")
+                arrayAdapter?.notifyDataSetChanged()
+                Log.d("LIST", "notified")
+                i++
+            }
+
+            override fun onScroll(p0: Float) {
+            }
+        })
+    }
+}
