151ec4bfa2f1c1dd25f9f5b3b838c2bdaacdc844
denisseai <msdenisseanaya@gmail.com>
2020-07-08 22:02:14 -0700

    login page wireframe

---
+++ app/src/main/java/com/example/walkingbuddiesdogedition/activities/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.walkingbuddiesdogedition.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.walkingbuddiesdogedition.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
---
+++ app/src/main/java/com/example/walkingbuddiesdogedition/activities/StartupActivity.kt
@@ -0,0 +1,22 @@
+package com.example.walkingbuddiesdogedition.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.walkingbuddiesdogedition.R
+import com.example.walkingbuddiesdogedition.R.layout.activity_startup
+
+class StartupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_startup)
+    }
+
+    fun onLogin(v: View){
+
+    }
+
+    fun onSignup(v: View){
+
+    }
+}
