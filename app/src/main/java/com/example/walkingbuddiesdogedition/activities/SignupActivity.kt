66a1926cc29d46f8a3f7616b91b661055fea8d63
denisseai <msdenisseanaya@gmail.com>
2020-07-10 20:42:04 -0700

    user can signup with email and password

--- app/src/main/java/com/example/walkingbuddiesdogedition/activities/SignupActivity.kt
+++ app/src/main/java/com/example/walkingbuddiesdogedition/activities/SignupActivity.kt
@@ -5,17 +5,48 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.walkingbuddiesdogedition.MainActivity
 import com.example.walkingbuddiesdogedition.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuth.AuthStateListener
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
+
+    private val firebaseAuth = FirebaseAuth.getInstance()
+    private val firebaseAuthListener = AuthStateListener {
+        val user = firebaseAuth.currentUser
+        if (user != null) {
+            startActivity(MainActivity.newIntent(this))
+            finish()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
     }
 
+    override fun onStart() {
+        super.onStart()
+        firebaseAuth.addAuthStateListener(firebaseAuthListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        firebaseAuth.removeAuthStateListener(firebaseAuthListener)
+    }
+
     fun onSignup(v: View) {
-        startActivity(MainActivity.newIntent(this))
+        if(!email.text.toString().isNullOrEmpty() && !password.text.toString().isNullOrEmpty()) {
+            firebaseAuth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener { task ->
+                    if (!task.isSuccessful) {
+                        Toast.makeText(this, "Signup error ${task.exception?.localizedMessage}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
     }
     companion object {
         fun newIntent(context: Context?) = Intent(context, SignupActivity::class.java)
